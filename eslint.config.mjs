0996a693fa6479808c1925bb1e1c25c5f9e623c4
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-05-25 17:52:28 +0200

    chore: updated linting and build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,192 @@
-import eslintConfigTrumpet from '@trumpet/eslint-config-next';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import eslintJS from '@eslint/js';
+import eslintPluginNode from 'eslint-plugin-n';
+import eslintPluginPerfectionist from 'eslint-plugin-perfectionist';
+import eslintPluginPromise from 'eslint-plugin-promise';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import eslintPluginVitest from 'eslint-plugin-vitest';
+import globals from 'globals';
+import eslintTS from 'typescript-eslint';
 
-export default [
-    ...eslintConfigTrumpet,
-    eslintConfigPrettier,
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default eslintTS.config(
+    eslintJS.configs.recommended,
+    ...eslintTS.configs.strictTypeChecked,
+    ...eslintTS.configs.stylisticTypeChecked,
+    eslintPluginNode.configs['flat/recommended-module'],
+    eslintPluginPromise.configs['flat/recommended'],
+    eslintPluginUnicorn.configs.recommended,
+    eslintPluginPerfectionist.configs['recommended-alphabetical'],
     {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: __dirname,
+            },
+        },
+        plugins: {
+            'unused-imports': eslintPluginUnusedImports,
+        },
         rules: {
-            'no-console': 'off',
+            '@typescript-eslint/array-type': ['error', { default: 'array' }],
+            '@typescript-eslint/consistent-indexed-object-style': 'error',
+            '@typescript-eslint/consistent-type-definitions': 'warn',
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    custom: {
+                        match: false,
+                        regex: '^[IT][A-Z]',
+                    },
+                    format: ['PascalCase'],
+                    selector: 'interface',
+                },
+                {
+                    custom: {
+                        match: false,
+                        regex: '^[IT][A-Z]',
+                    },
+                    format: ['PascalCase'],
+                    selector: 'typeAlias',
+                },
+                {
+                    format: ['PascalCase'],
+                    selector: 'enumMember',
+                },
+                {
+                    custom: {
+                        match: true,
+                        regex: '^[A-Z]$',
+                    },
+                    format: ['PascalCase'],
+                    selector: 'typeParameter',
+                },
+            ],
+            '@typescript-eslint/no-extra-non-null-assertion': 'error',
+            '@typescript-eslint/no-floating-promises': [
+                'error',
+                { ignoreIIFE: true, ignoreVoid: true },
+            ],
+            '@typescript-eslint/no-for-in-array': 'error',
+            '@typescript-eslint/no-inferrable-types': 'error',
+            '@typescript-eslint/no-non-null-assertion': 'off',
+            '@typescript-eslint/no-redundant-type-constituents': 'warn',
+            '@typescript-eslint/no-require-imports': 'warn',
+            '@typescript-eslint/no-this-alias': 'error',
+            '@typescript-eslint/no-unnecessary-boolean-literal-compare':
+                'error',
+            '@typescript-eslint/no-unnecessary-condition': 'error',
+            '@typescript-eslint/no-unnecessary-qualifier': 'warn',
+            '@typescript-eslint/no-unnecessary-type-arguments': 'error',
+            '@typescript-eslint/no-unused-expressions': 'warn',
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { args: 'all', argsIgnorePattern: '^_' },
+            ],
+            '@typescript-eslint/no-useless-constructor': 'warn',
+            '@typescript-eslint/no-useless-empty-export': 'warn',
+            '@typescript-eslint/prefer-as-const': 'warn',
+            '@typescript-eslint/prefer-for-of': 'warn',
+            '@typescript-eslint/prefer-includes': 'warn',
+            '@typescript-eslint/prefer-nullish-coalescing': 'error',
+            '@typescript-eslint/prefer-optional-chain': 'error',
+            '@typescript-eslint/require-await': 'error',
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/switch-exhaustiveness-check': 'warn',
+            'curly': 'error',
+            'eqeqeq': 'error',
+            'n/no-extraneous-import': 'off',
+            'n/no-missing-import': 'off',
+            'n/no-process-exit': 'error',
+            'n/no-unsupported-features/node-builtins': 'off',
+            'no-console': 'warn',
+            'no-unused-vars': 'off',
+            'object-shorthand': 'error',
+            'perfectionist/sort-exports': [
+                'error',
+                {
+                    order: 'asc',
+                    type: 'natural',
+                },
+            ],
+            'perfectionist/sort-imports': 'off',
+            'perfectionist/sort-named-imports': [
+                'error',
+                {
+                    order: 'asc',
+                    type: 'natural',
+                },
+            ],
+            'prefer-const': ['error', { destructuring: 'all' }],
+            'prefer-destructuring': 'error',
+            'prefer-template': 'warn',
+            'unicorn/catch-error-name': 'off',
+            'unicorn/explicit-length-check': 'off',
+            'unicorn/filename-case': 'off',
+            'unicorn/new-for-builtins': 'off',
+            'unicorn/no-array-callback-reference': 'off',
+            'unicorn/no-array-for-each': 'off',
+            'unicorn/no-array-reduce': 'off',
+            'unicorn/no-nested-ternary': 'off',
+            'unicorn/no-null': 'off',
+            'unicorn/no-process-exit': 'off',
+            'unicorn/no-useless-undefined': 'off',
+            'unicorn/prefer-module': 'off',
+            'unicorn/prefer-string-raw': 'off',
+            'unicorn/prevent-abbreviations': 'off',
+            'unused-imports/no-unused-imports': 'error',
         },
     },
-];
+    {
+        extends: [eslintTS.configs.disableTypeChecked],
+        files: ['**/*.js', '**/*.cjs', '**/*.mjs', 'eslint.config.mjs'],
+    },
+    {
+        files: ['**/*.spec.{ts,tsx}'],
+        plugins: {
+            vitest: eslintPluginVitest,
+        },
+        rules: {
+            ...eslintPluginVitest.configs.recommended.rules,
+            '@typescript-eslint/ban-ts-comment': 'off',
+            '@typescript-eslint/no-confusing-void-expression': 'off',
+            '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-floating-promises': 'off',
+            '@typescript-eslint/no-implied-eval': 'off',
+            '@typescript-eslint/no-magic-numbers': 'off',
+            '@typescript-eslint/no-misused-promises': [
+                'error',
+                {
+                    checksVoidReturn: false,
+                },
+            ],
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-call': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            '@typescript-eslint/no-var-requires': 'off',
+            '@typescript-eslint/prefer-as-const': 'off',
+            '@typescript-eslint/require-await': 'off',
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/unbound-method': 'off',
+            'import-x/no-named-as-default': 'off',
+            'import-x/no-named-as-default-member': 'off',
+            'prefer-rest-params': 'off',
+            'react-perf/jsx-no-new-array-as-prop': 'off',
+            'unicorn/consistent-function-scoping': 'off',
+            'unicorn/error-message': 'off',
+            'unicorn/no-await-expression-member': 'off',
+        },
+    },
+    {
+        ignores: ['out', 'dist', 'node_modules', 'target', 'coverage'],
+    },
+);
---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import path from 'node:path';
+
+import { PackageJson } from 'type-fest';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+import manifest from './package.json';
+
+const { name } = manifest as PackageJson;
+export default defineConfig({
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/index.ts'),
+            fileName: (format) => `index.${format === 'es' ? 'mjs' : 'js'}`,
+            formats: ['es', 'cjs'],
+            name,
+        },
+        minify: true,
+        rollupOptions: {
+            output: {
+                preserveModules: false,
+            },
+        },
+        sourcemap: true,
+        target: 'es2020',
+    },
+    plugins: [dts({ rollupTypes: true })],
+    test: {
+        coverage: {
+            exclude: ['node_modules/**', 'dist/**'],
+            reporter: ['text', 'lcov'],
+        },
+        globals: true,
+        include: ['tests/**/*.spec.ts'],
+    },
+});
